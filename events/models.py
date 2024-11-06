4e8561f62e3e47b16de8913208371dededd7df8b
Dhe-Reigner <martinkagema01@gmail>
2024-11-06 15:16:37 +0300

    Initial commit

--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    name = models.CharField('Venue Name', max_length=120)
+    address = models.CharField(max_length=300)
+    zip_code = models.CharField('Zip_code', max_length=20)
+    phone = models.CharField('Contact Phone', max_length=20)
+    web = models.URLField('Website Address', )
+    email_address = models.EmailField()
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    venue = models.CharField(max_length=120)
+    manager = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+
