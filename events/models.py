1df96a612edab6b92cb85c30e5653643588a3439
Martin Kagema <martinkagema01@gmail.com>
2024-11-07 06:15:36 +0300

    Initial commit

--- events/models.py
+++ events/models.py
@@ -6,12 +6,29 @@ class Venue(models.Model):
     zip_code = models.CharField('Zip_code', max_length=20)
     phone = models.CharField('Contact Phone', max_length=20)
     web = models.URLField('Website Address', )
-    email_address = models.EmailField()
+    email_address = models.EmailField('Email Address')
+
+    def __str__(self):
+        return self.name
+
+class MyClubUser(models.Model):
+     first_name = models.CharField('First Name', max_length=120)
+     last_name = models.CharField('Last Name', max_length=120)
+     email = models.EmailField('Email Address')
+
+     def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
-    venue = models.CharField(max_length=120)
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    # venue = models.CharField(max_length=120)
     manager = models.CharField(max_length=60)
     description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
+
+    def  __str__(self):
+        return self.name
 
